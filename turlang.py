eb928c8bdc593bd91a944f1024fdbda286fbaa47
Michael-Bianconi <michael.a.bianconi@gmail.com>
2020-01-02 11:19:44 -0500

    Added tape limit option

--- turlang.py
+++ turlang.py
@@ -3,17 +3,21 @@ import sys, re, functools, operator, argparse
 ignoreregex = re.compile("^(//.*|"")$", re.IGNORECASE)
 labelregex = re.compile("^\\S+:$")
 transitionregex = re.compile(
-    "^(\\S|any|end|space|newline|tab|baz)\\s+->" +
-    "\\s+(\\S|end|none|space|newline|tab|baz)" +
+    "^(\\S|end|spc|nl|tab)\\s+->" +
+    "\\s+(\\S|end|none|spc|nl|tab)" +
     "\\s+(left|right)" +
     "\\s+\\S+$", re.IGNORECASE)
 states = {}
 tape = []
 head = 1
 linecount = -1
+debug = False
+tapelength = None
 
-def err(msg: str):
-    print("Error (line " + str(linecount) + "): ", end='')
+def err(msg: str, showline=True):
+    global linecount
+    line = '' if linecount == -1 else '(line ' + str(linecount) + ')'
+    if showline: print("Error " + line, end='')
     print(msg)
     sys.exit(1)
 
@@ -27,7 +31,9 @@ def read():
     else: return "end"
     
 def write(value):
-    global tape, head
+    global tape, head, tapelength
+    if len(tape) > tapelength:
+        err('Tape length exceeds limit', showline=False)
     if head >= 0 and head < len(tape): tape[head] = value
     elif head >= 0:
         tape.append("end")
@@ -40,19 +46,19 @@ def write(value):
     
 def create_transition(states, label, read, write, tape, next):
 
-    readkeywords = ['any','baz','newline','space','tab']
-    writekeywords = ['none','baz','newline','space','tab']
+    readkeywords = ['nl','spc','tab']
+    writekeywords = ['none','nl','spc','tab']
     nextkeywords = ['accept','reject']
     if read.lower() in readkeywords: read = read.lower()
     if write.lower() in writekeywords: write = write.lower()
     if next.lower() in nextkeywords: next = next.lower()
     tape = tape.lower()
     
-    if read == "newline": read = '\n'
-    elif read == "space": read = ' '
+    if read == "nl": read = '\n'
+    elif read == "spc": read = ' '
     elif read == "tab": read = '\t'
-    if write == "newline": write = '\n'
-    elif write == "space": write = ' '
+    if write == "nl": write = '\n'
+    elif write == "spc": write = ' '
     elif write == "tab": write = '\t'
     
     if label not in states:
@@ -66,36 +72,45 @@ def parse(filename: str) -> tuple:
     global states, linecount
     active = None
     start = None
-    
-    with open(filename) as tlfile:
-        for line in tlfile:
-            line = line.strip()
-            linecount += 1
-            if (ignoreregex.match(line)): continue
-            elif (labelregex.match(line)):
-                active = line[:-1]
-                states[active] = {}
-                if start is None: start = active
-            elif (transitionregex.match(line)):
-                line = line.split()
-                line.pop(1)  # remove arrow
-                create_transition(states, active, *line)
-            else:
-                err('Syntax error')
-                sys.exit(1)
-    return start
+    try:
+        with open(filename) as tlfile:
+            for line in tlfile:
+                line = line.strip()
+                linecount += 1
+                if (ignoreregex.match(line)): continue
+                elif (labelregex.match(line)):
+                    active = line[:-1]
+                    if active.lower() in ['accept', 'reject']:
+                        err('Cannot redefine halt state ' + active)
+                    if active in states:
+                        err('State redefinition: ' + active)
+                    states[active] = {}
+                    if start is None: start = active
+                elif (transitionregex.match(line)):
+                    line = line.split()
+                    line.pop(1)  # remove arrow
+                    create_transition(states, active, *line)
+                else:
+                    err('Syntax error')
+                    sys.exit(1)
+        return start
+    except FileNotFoundError:
+        err("Could not find file")
+    except IOError:
+        err("Could not open file")
 
 def transition(statelabel, read):
-    global states, head
+    global states, head, debug
     if statelabel not in states:
         err('Jump to undefined state: ' + str(statelabel))
     state = states[statelabel]
-    if "any" in state: read = "any"
     if not read in state: return None
     trans = state[read]
     if trans["write"] != "none": write(trans["write"])
     if trans["tape"] == "left": head -= 1
     elif trans["tape"] == "right": head += 1
+    if debug:
+        print('Moving to ' + trans["next"])
     return trans["next"]
         
 
@@ -108,15 +123,19 @@ def simulate(active):
         elif active == "accept": return True
 
 def main():
-    global states, tape
+    global states, tape, debug, tapelength
     
     parser = argparse.ArgumentParser(description='Turlang Interpreter')
     parser.add_argument('source', type=str, help='Turlang Source')
     parser.add_argument('string', type=str, help='Input string')
+    parser.add_argument('--tapelength', type=int, help='Maximum tape length')
+    parser.add_argument('-d', action='store_true')
     args = parser.parse_args()
     
+    debug = args.d
     filename = args.source
     start = parse(args.source)
+    tapelength = args.tapelength
     create_tape(args.string)
     
     if start is None:
